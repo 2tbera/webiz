6d3e19783e52ebbf52bfda54667392c4eed96447
macbook <gtutberidze@sharemac.de>
2022-12-07 20:38:59 +0400

    commit

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -6,13 +6,14 @@ const api = async (req, res) => {
 /**
  * @TODO logic of exchange 
 */    
-
  request({
     method: 'GET',
     uri: `https://api.apilayer.com/currency_data/convert?from=${req.query.from_currency_code}&to=${req.query.to_currency_code}&amount=${(req.query.amount / 100)}`,
     headers: {'apikey': 'tdXAFB0cavDWRP7IyHgv2tmOlkkmuvQe'}
   } , function (error, response, body){
+
     if(!error && response.statusCode == 200){
+
       const api = JSON.parse(body);
       const data = { 
          exchange_rate: Math.round(api.info.quote * 1000) / 1000 ,
